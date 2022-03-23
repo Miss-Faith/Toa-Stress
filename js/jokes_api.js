fba7794cf2a993833829303d4ec13a0f22f12b3d
annomao <omaoannkerubo@gmail.com>
2022-03-23 23:51:32 +0300

    add laugh emoji

--- js/jokes_api.js
+++ js/jokes_api.js
@@ -4,14 +4,17 @@ let jokeButton = document.getElementById("get-joke");
 getJoke();
 
 jokeButton.addEventListener("click", getJoke);
+
 function getJoke() {
   fetch("http://icanhazdadjoke.com", {
   headers: {
     Accept: "application/json",
   }})
+
  .then(response => response.json())
  .then(data => {
    jokeText.innerHTML = data.joke;
  });
+ 
 }
  
