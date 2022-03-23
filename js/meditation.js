00dc3bc6c19a94b9d4d657e39b89b65d80fe5750
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-03-23 22:45:06 +0300

    Add particles and rotating ball feature

--- js/meditation.js
+++ js/meditation.js
@@ -1,18 +1,18 @@
-let totalTime = 7500;
+let totalTime = 8000;
 let breathTime = (totalTime * 0.4);
 let holdTime = (totalTime * 0.2);
 
 function breathAnimation () {
 
-  $(".main-container").addClass("grow");
-  $(".breath").text('Breath In!');
+  $(".main-container").addClass("grow").removeClass("shrink");
+  $("#breath").text('Breath In!');
 
   setTimeout (function() {
-    $(".breath").text('Hold');
+    $("#breath").text('Hold');
 
     setTimeout (function() {
-    $(".breath").text('Breath Out!');
-    $(".main-container").addClass("shrink");
+    $("#breath").text('Breath Out!');
+    $(".main-container").removeClass("grow").addClass("shrink");
     }, holdTime);
   }, breathTime);
 }
