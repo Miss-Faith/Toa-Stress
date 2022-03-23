6dad252a3598817160ed820a5659a4685cca6da9
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-03-23 15:37:49 +0300

    Add breath function

--- js/meditation.js
+++ js/meditation.js
@@ -1,1 +1,20 @@
-. 
+let totalTime = 7500;
+let breathTime = (totalTime * 0.4);
+let holdTime = (totalTime * 0.2);
+
+function breathAnimation () {
+
+  $(".main-container").addClass("grow");
+  $(".breath").text('Breath In!');
+
+  setTimeout (function() {
+    $(".breath").text('Hold');
+
+    setTimeout (function() {
+    $(".breath").text('Breath Out!');
+    $(".main-container").addClass("shrink");
+    }, holdTime);
+  }, breathTime);
+}
+
+setInterval(breathAnimation, totalTime);
