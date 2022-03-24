776c08eaff6d4e826eaf5b1d51f1864d82e5b169
VincentGichohi <vincentegichohi@gmail.com>
2022-03-24 13:35:01 +0300

    Added constants to link html to javascript

--- js/breakout.js
+++ js/breakout.js
@@ -1,1 +1,6 @@
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close');
+const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
