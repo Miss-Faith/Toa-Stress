3029498a0c8efda14f13b79e623ee1ea2510e952
VincentGichohi <vincentegichohi@gmail.com>
2022-03-24 14:09:56 +0300

    Added a function for drawing the ball to the canvas

--- js/breakout.js
+++ js/breakout.js
@@ -52,3 +52,14 @@ const bricks = [];
       bricks[i][j] = { x, y, ...brickInfo };
     }
   }
+
+  //a function for drawing the ball to the canvas
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+    ctx.fillStyle = ball.visible ? '#0095dd' : 'transparent';
+    ctx.fill();
+    ctx.closePath();
+}
+
+  
