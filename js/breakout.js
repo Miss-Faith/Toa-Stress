1a2d60f3ac1799022b86678c94dceae9f7a2188e
VincentGichohi <vincentegichohi@gmail.com>
2022-03-24 14:07:48 +0300

    created an array for storing brick data

--- js/breakout.js
+++ js/breakout.js
@@ -41,3 +41,14 @@ const brickInfo = {
     offsetY: 60,
     visible: true
   };
+
+//setting an empty array for storing data from the bricks
+const bricks = [];
+  for (let i = 0; i < brickRowCount; i++) {
+    bricks[i] = [];
+    for (let j = 0; j < brickColumnCount; j++) {
+      const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+      const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+      bricks[i][j] = { x, y, ...brickInfo };
+    }
+  }
